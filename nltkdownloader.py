53595562a2f95549d61c2bef1f776250b5a29c3d
Kunj Joshi <93201673+KunjJoshi@users.noreply.github.com>
2023-10-29 16:56:12 +0530

    Downloads

--- nltkdownloader.py
+++ nltkdownloader.py
@@ -1,4 +1,9 @@
 import nltk
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+
+model_name="jonatasgrosman/wav2vec2-large-xlsr-53-english"
+model=Wav2Vec2ForCTC.from_pretrained(model_name)
+processor=Wav2Vec2Processor.from_pretrained(model_name)
 
 nltk.download('words')
 nltk.download('punkt')
